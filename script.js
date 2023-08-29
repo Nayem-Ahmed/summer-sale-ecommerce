0a93369c64c8b44a1914c7cae839d911478d24c7
nayem ahmed <freelancernayem.me@gmail.com>
2023-08-29 19:12:03 +0600

    first commit

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let total = 0;
 const discountPrice = document.getElementById("discount-price");
 const totalPrice = document.getElementById("total-price");
 const totalElement = document.getElementById("total");
+const KAccessoriesTitle = document.getElementById("K-Accessories-title").innerText;
 
 const couponInput = document.getElementById("couponInput");
 const applyCouponButton = document.getElementById("applyCouponButton");
@@ -21,20 +22,14 @@ function pickPrice(card) {
     const purchaseButton = document.getElementById("purchaseButton");
     purchaseButton.disabled = !(subTotal >= 200); 
     // ...
+    const li = document.createElement("li");
+    const text = document.createTextNode(KAccessoriesTitle);
+    li.appendChild(text);
+    const div = document.getElementById("show-list");
+    div.appendChild(li);
 }
 
 
-
-
-// function applyCoupon() {
-//     if (couponInput.value === "SELL200") {
-//         discount = (subTotal * 0.2);
-//         discountPrice.innerText = discount.toFixed(2);
-//         const totalWithDiscount = subTotal - discount; 
-//         totalPrice.innerText = totalWithDiscount.toFixed(2);
-//         couponInput.value = "";
-//     }
-// }
 
 applyCouponButton.addEventListener("click",function(){
     if (couponInput.value === "SELL200") {
